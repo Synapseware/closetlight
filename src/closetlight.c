8cc0ac36724f0a12b75d5d3f2fe5f365b037980f
Matthew Potter <matthew@synapseware.com>
2014-11-02 20:23:09 -0800

    Updating closet light behavior so it has timeouts and effects if left open for too long

--- src/closetlight.c
+++ src/closetlight.c
@@ -48,6 +48,35 @@ void configurePWM(void)
 
 
 //-----------------------------------------------------------------------------
+// ADC is used in free-running mode to generate random-ish numbers
+void configureAdc(void)
+{
+	ADMUX	=	(1<<REFS0)	|		// internal v-ref
+				(0<<ADLAR)	|		// don't left-adjust result
+				(0<<MUX1)	|		// sample from PB2 (MUX1:0 = 01)
+				(1<<MUX0);
+
+	ADCSRA	=	(1<<ADEN)	|
+				(1<<ADSC)	|
+				(1<<ADATE)	|		// we want to use free-running to enable autotriggering
+				(0<<ADIF)	|
+				(0<<ADIE)	|
+				(1<<ADPS2)	|		// overclock ADC so it's noisy
+				(0<<ADPS1)	|
+				(1<<ADPS0);
+
+	ADCSRB	=	(0<<ADTS2)	|		// ADTS2:0 = 0 for free running mode
+				(0<<ADTS1)	|
+				(0<<ADTS0);
+
+	DIDR0	=	(1<<ADC1D);			// PB2 is left floating on purpose
+	DDRB	&=	~(1<<PB2);			// make sure PB2 is in input mode
+	PINB	&=	~(1<<PB2);			// make sure pullup resistor is off
+	PORTB	&=	~(1<<PB2);
+}
+
+
+//-----------------------------------------------------------------------------
 // prepare device
 void init(void)
 {
@@ -82,31 +111,200 @@ uint8_t isDoorOpen(void)
 // Sets the PWM brightness level based on the door state
 void setLightLevel(uint8_t doorState)
 {
-	static uint8_t brightness	= 0;
-	static uint8_t stepDelay	= 0;		// max is FADE_STEP_MS
-	static uint8_t fadePtr		= 0;
+	static uint8_t
+		stepDelay	= 0,			// max is FADE_STEP_MS
+		fadeVal		= BRIGHT_OFF,	// mathmatically determined brightness value
+		brightness	= 0,			// the current brightness value
+		effect		= EFFECT_OFF;	// which effect are we on
 
-	// delay the processing of any fade steps
-	if (++stepDelay < FADE_STEP_MS)
+	static uint16_t
+		secondsOn	= 0,			// total # of seconds for each effect
+		ticks		= 0,			// # of ticks (1/1000 second)
+		delay		= 0;			// each effect can have it's own internal delays
+
+	// increment the seconds counter
+	if (ticks++ > 1000)
+	{
+		ticks = 0;
+		if (doorState)
+			secondsOn++;
+	}
+
+	// if the door is closed, then we're off
+	if (!doorState)
+		effect = EFFECT_OFF;
+
+	// always delay the processing of any brightness changes
+	if (stepDelay++ < FADE_STEP_MS)
 		return;
 	stepDelay = 0;
 
-	// handle tasks per MS window
-	if (doorState)
+	// Effects state engine
+	switch (effect)
 	{
-		// increase when door is open
-		if (fadePtr < FADE_TABLE_LEN - 1)
-			fadePtr++;
-	}
-	else
-	{
-		// decrease when door is closed
-		if (fadePtr > 0)
-			fadePtr--;
+		case EFFECT_OFF:
+			if (doorState)
+			{
+				secondsOn = 0;
+				effect = EFFECT_NORMAL;
+			}
+
+			fadeVal = 0;
+			break;
+
+		case EFFECT_NORMAL:
+			if (secondsOn > ONTIME_NORMAL)
+			{
+				effect = EFFECT_DIMMED;
+				secondsOn = 0;
+			}
+
+			// set fadeVal based on door state
+			fadeVal = BRIGHT_NORMAL;
+			break;
+
+		case EFFECT_DIMMED:
+			if (secondsOn > ONTIME_DIMMED)
+			{
+				effect = EFFECT_LOW;
+				secondsOn = 0;
+			}
+
+			// steady mediaum brightness
+			fadeVal = BRIGHT_MED;
+			break;
+
+		case EFFECT_LOW:
+			if (secondsOn > ONTIME_LOW)
+			{
+				effect = EFFECT_BLINK;
+				secondsOn = 0;
+			}
+
+			// steady low brightness
+			fadeVal = BRIGHT_LOW;
+			break;
+
+		case EFFECT_BLINK:
+		{
+			static uint8_t blink_state = 0;
+
+			if (secondsOn > ONTIME_BLINK)
+			{
+				effect = EFFECT_SLEEPY;
+				secondsOn = 0;
+			}
+
+			// this effect has it's own delays
+			if (delay)
+			{
+				delay--;
+			}
+			else
+			{
+				switch (blink_state)
+				{
+					// long low to fast bright
+					case 0:
+						fadeVal = BRIGHT_LOW;
+						delay = ADCH | (ADCL << 3) | (TCNT0 << 3);
+						blink_state = 1;
+						break;
+
+					// fast bright to off pause
+					case 1:
+						fadeVal = BRIGHT_NORMAL;
+						delay = 1;
+						blink_state = 2;
+						break;
+
+					// off pause to fast bright
+					case 2:
+						fadeVal = BRIGHT_OFF;
+						delay = ADCH | (ADCL << 3) + 53;
+						blink_state = 3;
+						break;
+
+					// fast bright
+					case 3:
+						fadeVal = BRIGHT_NORMAL;
+						delay = 15;
+						blink_state = (TCNT0 & 0x01) | (ADCH & 0x01)
+							? 4
+							: 0;
+						break;
+
+					// quick medium, random out to off pause or long low
+					case 4:
+						fadeVal = BRIGHT_MED;
+						delay = 3 + (TCNT0 & 0x0F);
+						blink_state = 0;
+						break;
+				}
+			}
+
+			brightness = fadeVal;
+		}
+		break;
+
+		case EFFECT_SLEEPY:
+		{
+			static uint8_t	sleepy_state = 0;
+
+			if (secondsOn > ONTIME_SLEEPY)
+			{
+				effect = EFFECT_OFF;
+				secondsOn = 0;
+			}
+
+			// sleepy eye effects...
+			if (delay)
+			{
+				delay--;
+			}
+			else
+			{
+				switch (sleepy_state)
+				{
+					// going up
+					case 2:
+						if (fadeVal < BRIGHT_LOW)
+							fadeVal += 4;
+						else
+							sleepy_state = 1;
+
+						delay = 5;
+						break;
+
+					// going down 
+					case 1:
+						if (fadeVal > 1)
+							fadeVal -= 2;
+						else
+							sleepy_state = 0;
+
+						delay = 5;
+						break;
+
+					// resting
+					case 0:
+						sleepy_state = 2;
+						fadeVal = 1;
+						delay = 420;
+						break;
+				}
+			}
+		}
+		break;
 	}
 
+	// adjust current brightness to match target brightness
+	if (brightness > fadeVal)
+		brightness -= FADE_STEPS;
+	else if (brightness < fadeVal)
+		brightness += FADE_STEPS;
+
 	// set new PWM value
-	brightness = pgm_read_byte(&FADE_TABLE[fadePtr]);
 	if (brightness > 0)
 		DDRB |= (1<<PWM_OUT);
 	else
@@ -126,7 +324,6 @@ int main(void)
 
 	while(1)
 	{
-
 		wdt_reset();
 		if (!_tick)
 			continue;
@@ -143,7 +340,7 @@ int main(void)
 // Compare-A interrupt handler
 ISR(TIM0_COMPA_vect)
 {
-	static timerTick = 0;
+	static uint8_t timerTick = 0;
 
 	if (timerTick++ < TICKS_MS)
 		return;
--- src/closetlight.h
+++ src/closetlight.h
@@ -16,18 +16,41 @@
 #define STATUS_LED		PB4
 
 #define PWM_LIMIT		200
+#define CLK_DIV			1
 
-const uint8_t			PWM_RANGE		= PWM_LIMIT-1;		// steps in PWM window
-const uint8_t			MAX_PWM			= PWM_LIMIT-1;
+const uint8_t			PWM_RANGE		= PWM_LIMIT - 1;		// steps in PWM window
+const uint8_t			MAX_PWM			= PWM_LIMIT - 1;
 
-const uint8_t			TICKS_MS		= (F_CPU / 1 / PWM_LIMIT / 1000);	// should be 48
-const uint8_t			FADE_STEP_MS	= 4;
+const uint8_t			FADE_STEP_MS	= 10;					// wait Xms between fade changes
+const uint8_t			FADE_STEPS		= 2;
 
+const uint8_t			TICKS_MS		= (F_CPU / CLK_DIV / PWM_LIMIT / 1000);	// should be 48
+const uint16_t			TICKS_S			= (F_CPU / CLK_DIV / PWM_LIMIT);
+
+
+#define					EFFECT_OFF		0
+#define					EFFECT_NORMAL	1
+#define					EFFECT_DIMMED	2
+#define					EFFECT_LOW		3
+#define					EFFECT_BLINK	4
+#define					EFFECT_SLEEPY	5
+
+#define					BRIGHT_NORMAL	220
+#define					BRIGHT_MED		80
+#define					BRIGHT_LOW		30
+#define					BRIGHT_OFF		0
+
+
+const uint16_t			ONTIME_NORMAL	= 120;
+const uint16_t			ONTIME_DIMMED	= 120;
+const uint16_t			ONTIME_LOW		= 120;
+const uint16_t			ONTIME_BLINK	= 300;
+const uint16_t			ONTIME_SLEEPY	= 600;
 
 
 const static uint8_t FADE_TABLE[] PROGMEM = {
 	0,
-	0,
+	0 /*,
 	0,
 	0,
 	0,
@@ -106,6 +129,7 @@ const static uint8_t FADE_TABLE[] PROGMEM = {
 	170,
 	185,
 	200
+	*/
 };
 const static uint8_t FADE_TABLE_LEN = sizeof(FADE_TABLE)/sizeof(uint8_t);
 
